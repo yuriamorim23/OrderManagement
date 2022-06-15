5fd61b017ea545ad9d0663969f39d557d383480f
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-06-15 18:47:47 +0000

    V.2

--- src/main/java/com/javaproject/ordermanagement/Entity/Client.java
+++ src/main/java/com/javaproject/ordermanagement/Entity/Client.java
@@ -3,10 +3,12 @@ package com.javaproject.ordermanagement.Entity;
 import java.io.Serializable;
 import java.util.UUID;
 
+
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private UUID id;
+	
 	private String firstName;
 	private String lastName;
 	private String address;
@@ -14,6 +16,87 @@ public class Client implements Serializable {
 	private String phoneNumber; 
 	private String email;
 	
+	
+	public Client(UUID id, String firstName, String lastName, String address, String postCode, String phoneNumber,
+			String email) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.postCode = postCode;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+
+	public UUID getId() {
+		return id;
+	}
+
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 }
--- src/main/java/com/javaproject/ordermanagement/Repositories/ClientRepository.java
+++ src/main/java/com/javaproject/ordermanagement/Repositories/ClientRepository.java
@@ -1,11 +1,13 @@
 package com.javaproject.ordermanagement.Repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.javaproject.ordermanagement.Entity.Client;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
-	
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+
 }
