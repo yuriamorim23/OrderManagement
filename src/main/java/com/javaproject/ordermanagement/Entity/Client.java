38d2ac16549dcd24c5ebc90024823ef0f7ecad0a
Yuri Amorim <yuricalixto.uk@gmail.com>
2022-06-19 16:21:30 +0000

    v.3

--- src/main/java/com/javaproject/ordermanagement/Entity/Client.java
+++ src/main/java/com/javaproject/ordermanagement/Entity/Client.java
@@ -1,14 +1,29 @@
 package com.javaproject.ordermanagement.Entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "client")
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	
+	@Column(unique = true)
+	private LocalDateTime created;
+	private LocalDateTime updated;
 	private String firstName;
 	private String lastName;
 	private String address;
@@ -16,10 +31,14 @@ public class Client implements Serializable {
 	private String phoneNumber; 
 	private String email;
 	
-	
-	public Client(UUID id, String firstName, String lastName, String address, String postCode, String phoneNumber,
-			String email) {
+	public Client() {}
+
+	public Client(UUID id, LocalDateTime created, LocalDateTime updated, String firstName, String lastName,
+			String address, String postCode, String phoneNumber, String email) {
+		super();
 		this.id = id;
+		this.created = created;
+		this.updated = updated;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
@@ -27,76 +46,94 @@ public class Client implements Serializable {
 		this.phoneNumber = phoneNumber;
 		this.email = email;
 	}
-
 
 	public UUID getId() {
 		return id;
 	}
 
-
 	public void setId(UUID id) {
 		this.id = id;
 	}
 
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
 
 	public String getFirstName() {
 		return firstName;
 	}
 
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 
 	public String getLastName() {
 		return lastName;
 	}
 
-
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
 
 	public String getAddress() {
 		return address;
 	}
 
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 
 	public String getPostCode() {
 		return postCode;
 	}
 
-
 	public void setPostCode(String postCode) {
 		this.postCode = postCode;
 	}
-
 
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
 
-
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-
 
 	public String getEmail() {
 		return email;
 	}
 
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
 	
 }
